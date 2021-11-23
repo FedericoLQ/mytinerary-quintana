fbe0a6dd6a90ef7b4c2ea1532c7b2a3ed65181fb
FedericoLQ <fedelima19@gmail.com>
2021-11-23 08:28:26 -0300

    Version 1.0

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,27 +11,22 @@ import Contact from "../assets/contact.png"
 const Footer = () => {
     return (
        
-            <footer >
-     
-          <div class="colum2">           
-            <div class="redes">
+            <footer >                   
+            <div className="m-5 container-footer">
             <div className="footL">
                  <Link as={Link} to="/" className="active footLinks">Home</Link>
                  <Link eventKey={2} as={Link} to="/Cities" className="footLinks">Cities</Link>
            
                 </div>
-                <div class="roww">                
+                <div className="imgFoot">                
                   <Link to="/"><img src={Facebook} alt="Logo Facebook"/></Link>
                   <Link to="/"><img src={Instagram} alt="Logo Instagram"/></Link>
                   <Link to="/"><img src={Youtube} alt="Logo Youtube"/></Link>
                   <Link to="/"><img src={Twitter} alt="Logo Twitter"/></Link>
                 </div>
-               
+               <h4 className="mt-4 text-center text-white">MyTinerary Proyect 2021 - All Rights Reserved</h4>
               
-            </div>
-          </div>
-                    
-      
+            </div>             
     </footer>
        
     )
