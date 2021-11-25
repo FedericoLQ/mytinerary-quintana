a15a54f9694c2ab8d28adbb341550826d65b48bc
FedericoLQ <fedelima19@gmail.com>
2021-11-25 15:04:01 -0300

    Server V 0.5

--- frontend/src/components/SimpleSlider.js
+++ frontend/src/components/SimpleSlider.js
@@ -1,46 +1,58 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from "react";
 import Slider from "react-slick";
 import { Container } from "react-bootstrap";
 import Image from "./Img";
 import { Card } from "react-bootstrap";
 
-export default class SimpleSlider extends Component {x 
-  render() {
-    const settings = {
-      className: "center",
-      centerMode: true,
-      slidesToShow: 1,
-      rows: 2,
-      centerPadding: "5px",
-      slidesPerRow: 2,
-      infinite: true,
-      dots: true,
-      speed: 500,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      slidesToScroll: 2,
-      pauseOnHover: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            slidesPerRow: 1,
-            rows: 2,
-            dots: false,
-            arrows: false,
-          },
+
+
+
+
+const SimpleSlider = () => {
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/cities")
+      .then((res) => res.json())
+      .then((data) => setCities(data.response.cities))
+      .catch((err) => console.error(err.message));
+  }, []);
+  const settings = {
+    className: "center",
+    centerMode: true,
+    slidesToShow: 1,
+    rows: 2,
+    centerPadding: "5px",
+    slidesPerRow: 2,
+    infinite: true,
+    dots: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToScroll: 2,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          slidesPerRow: 1,
+          rows: 2,
+          dots: false,
+          arrows: false,
         },
-      ],
-    };
-    return (
+      },
+    ],
+  };
+  
+  return (
+    
       <Container fluid>
         <div className="carrusell">
           <Container className="containerCarrousel">
             <h2 className="text-center p-2 border pb-3 border-5 border-white pb-2 mt-3 rounded-pill text-white">Fulfill Your Traveling <span>Dreams</span></h2>
             <Slider {...settings}>
-              {Image.map((img, index) => {
+              {cities.map((img, index) => {
                 return (
                   <div key={index}>
                     <Card className="m-2 border border-2 border-white bg-dark text-white  text-center">
@@ -61,6 +73,8 @@ export default class SimpleSlider extends Component {x
           </Container>
         </div>
       </Container>
-    );
-  }
+    
+  )
 }
+export default SimpleSlider
+
