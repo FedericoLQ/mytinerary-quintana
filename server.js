73f62a4de5d421c5e2e9c2502f4fe6789cd90903
FedericoLQ <fedelima19@gmail.com>
2022-01-04 09:54:33 -0300

    heroku ajust

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-require("dotenv").config();
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
-const router = require("./routes/routes");
-const passport = require("passport");
+const Router = require('./routes/routes')
+const passport = require("passport")
+require('./config/database')
+const app = express()
+
 require("./config/database");
+const path = require("path");
 
-const app = express();
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
-app.use(cors());
+// middlewares
+app.use(cors())
 app.use(express.json());
-app.use(passport.initialize());
+app.use(passport.initialize())
+app.use('/api', Router)
 
+const port = process.env.PORT || 4000;
 
-app.use("/api", router);
-app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0' ,()=> console.log("Server is listening on port"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+  }
+
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`El server esta en el puerto ${port}`);
+  });
